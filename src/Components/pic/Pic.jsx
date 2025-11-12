9be41db842a55c76a8bb027cb8209dc03c7695c5
Sabbir-75 <mdsabbirhossain9200@gmail.com>
2025-11-13 03:22:49 +0600

    skill icon change in my pic section

--- src/Components/pic/Pic.jsx
+++ src/Components/pic/Pic.jsx
@@ -1,18 +1,17 @@
 "use client";
 
 import React, { useEffect, useState, memo } from 'react';
+import typescript from "../../assets/typescript.png"
+import mongodb from "../../assets/mongodb.png"
+import next from "../../assets/next.jpg"
 
 const iconComponents = {
-    html: {
-        component: () => <svg viewBox="0 0 24 24" fill="currentColor" className="w-full h-full">
-            <path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z" fill="#E34F26" />
-        </svg>,
+    next: {
+        component: () => <img src={next} alt={next} /> ,
         color: '#E34F26'
     },
-    css: {
-        component: () => <svg viewBox="0 0 24 24" fill="currentColor" className="w-full h-full">
-            <path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.751L12 19.351l5.379-1.443.744-8.157z" fill="#1572B6" />
-        </svg>,
+    typescript: {
+        component: () => <img src={typescript} alt={typescript} />  ,
         color: '#1572B6'
     },
     javascript: {
@@ -39,10 +38,8 @@ const iconComponents = {
         </svg>,
         color: '#339933'
     },
-    tailwind: {
-        component: () => <svg viewBox="0 0 24 24" fill="currentColor" className="w-full h-full">
-            <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" fill="#06B6D4" />
-        </svg>,
+    mongodb: {
+        component: () => <img src={mongodb} alt={mongodb} />  ,
         color: '#06B6D4'
     }
 };
@@ -54,23 +51,23 @@ const SkillIcon = memo(({
 });
 SkillIcon.displayName = 'SkillIcon';
 const skillsConfig = [{
-    id: 'html',
+    id: 'next',
     orbitRadius: 135,
-    size: 40,
+    size: 50,
     speed: 1,
-    iconType: 'html',
+    iconType: 'next',
     phaseShift: 0,
     glowColor: 'cyan',
-    label: 'HTML5'
+    label: 'Next.js'
 }, {
-    id: 'css',
+    id: 'typescript',
     orbitRadius: 135,
     size: 45,
     speed: 1,
-    iconType: 'css',
+    iconType: 'typescript',
     phaseShift: 2 * Math.PI / 3,
     glowColor: 'cyan',
-    label: 'CSS3'
+    label: 'TypeScript'
 }, {
     id: 'javascript',
     orbitRadius: 135,
@@ -99,14 +96,14 @@ const skillsConfig = [{
     glowColor: 'purple',
     label: 'Node.js'
 }, {
-    id: 'tailwind',
+    id: 'mongodb',
     orbitRadius: 180,
-    size: 40,
+    size: 55,
     speed: -0.6,
-    iconType: 'tailwind',
+    iconType: 'mongodb',
     phaseShift: 4 * Math.PI / 3,
     glowColor: 'purple',
-    label: 'Tailwind CSS'
+    label: 'MongoDB'
 }];
 const OrbitingSkill = memo(({
     config,
